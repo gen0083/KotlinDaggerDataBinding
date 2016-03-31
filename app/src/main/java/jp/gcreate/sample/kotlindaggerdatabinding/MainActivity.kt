0fd0c685ed1622fd1dcfe70149937c5667c5206b
Gen <archiherewego@gmail.com>
2016-03-31 12:54:49 +0900

    WIP provide data binding instance by dagger2

--- app/src/main/java/jp/gcreate/sample/kotlindaggerdatabinding/MainActivity.kt
+++ app/src/main/java/jp/gcreate/sample/kotlindaggerdatabinding/MainActivity.kt
@@ -1,10 +1,8 @@
 package jp.gcreate.sample.kotlindaggerdatabinding
 
-import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import jp.gcreate.sample.kotlindaggerdatabinding.databases.OrmaDatabase
 import jp.gcreate.sample.kotlindaggerdatabinding.databases.TestData
 import jp.gcreate.sample.kotlindaggerdatabinding.databinding.ActivityMainBinding
 import javax.inject.Inject
@@ -17,25 +15,26 @@ class MainActivity : AppCompatActivity() {
     @field:[Inject Named("contextString")]
     lateinit var contextString: String
 
-    @Inject
-    lateinit var orma: OrmaDatabase
+//    @Inject
+//    lateinit var orma: OrmaDatabase
 
     @Inject
     lateinit var data: TestData
 
-    private lateinit var binding: ActivityMainBinding
+    @Inject
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+//        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         CustomApp.getComponent(this).inject(this)
         binding.simpleText.text = injectedString
         binding.simpleText.text = contextString
         Log.d("test", "from dagger " + data.toString())
 
-        orma.insertIntoTestData(TestData(1, "hoge"))
-        orma.selectFromTestData()
-                .executeAsObservable()
-                .subscribe { testData -> Log.d("test", "read from db id:" + testData.id + " name:" + testData.name) }
+//        orma.insertIntoTestData(TestData(1, "hoge"))
+//        orma.selectFromTestData()
+//                .executeAsObservable()
+//                .subscribe { testData -> Log.d("test", "read from db id:" + testData.id + " name:" + testData.name) }
     }
 }
--- app/src/main/java/jp/gcreate/sample/kotlindaggerdatabinding/di/AppModule.kt
+++ app/src/main/java/jp/gcreate/sample/kotlindaggerdatabinding/di/AppModule.kt
@@ -1,10 +1,13 @@
 package jp.gcreate.sample.kotlindaggerdatabinding.di
 
 import android.content.Context
+import android.databinding.DataBindingUtil
+import android.view.LayoutInflater
 import dagger.Module
 import dagger.Provides
-import jp.gcreate.sample.kotlindaggerdatabinding.databases.OrmaDatabase
+import jp.gcreate.sample.kotlindaggerdatabinding.R
 import jp.gcreate.sample.kotlindaggerdatabinding.databases.TestData
+import jp.gcreate.sample.kotlindaggerdatabinding.databinding.ActivityMainBinding
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -39,9 +42,14 @@ class AppModule(private val context: Context) {
         return TestData(10, "from Dagger2")
     }
 
+//    @Provides
+//    @Singleton
+//    fun provideOrmaDatabase(context: Context): OrmaDatabase {
+//        return OrmaDatabase.builder(context).build()
+//    }
+
     @Provides
-    @Singleton
-    fun provideOrmaDatabase(context: Context): OrmaDatabase {
-        return OrmaDatabase.builder(context).build()
+    fun provideActivityMainBinding(context: Context): ActivityMainBinding{
+        return DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.activity_main, null, false)
     }
 }
